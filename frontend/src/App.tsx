d592aeabab8425cb8383e03c2c849c39fe932dde
p0tat0chip <mr.mayankgame@gmail.com>
2025-02-16 17:12:37 +0530

    add that car animation

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { CarScene } from "./components/CarScene";
+import "./styles.css"; // Import the new CSS file
+
+export default function App() {
+  return (
+    <main>
+      <div className="car-scene-container">
+        <CarScene />
+      </div>
+      <div className="heading">
+        <div className="bg-primary">Need</div>
+        <div className="bg-primary">parking?</div>
+      </div>
+    </main>
+  );
+}
